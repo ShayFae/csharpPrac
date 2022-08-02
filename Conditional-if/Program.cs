4d9312f10abdb14191574c261d9a9c61ac548085
ShayFae <ifrahnoor108@gmail.com>
2022-08-02 10:28:04 -0400

    Conditionals

---
+++ Conditional-if/Program.cs
@@ -0,0 +1,13 @@
+﻿int theVal = 55;
+
+//If-else
+if(theVal == 50) {
+  Console.WriteLine("theVal is 50");
+} else if(theVal >= 51 && theVal <= 60) {
+    Console.WriteLine("theVal is between 51 and 60");
+  } else {
+  Console.WriteLine("theVal equals something else");
+}
+
+//Ternary Operator
+Console.WriteLine(theVal < 50 ? "theVal is small" : "theVal is large");
---
+++ Conditional-switch/Program.cs
@@ -0,0 +1,18 @@
+﻿int newVal = 50;
+
+switch(newVal) {
+  case 50:
+    Console.WriteLine("This value equals 50");
+    break;
+  case 51:
+    Console.WriteLine("This value equals 51");
+    break;
+  case 52:
+  case 53:
+  case 54:
+    Console.WriteLine("This value is between 52 to 54");
+    break;
+  default:
+    Console.WriteLine("The value equals something else");
+    break;
+}
