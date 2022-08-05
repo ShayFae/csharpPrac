5ea6278b1b89ebb10b289f9401db775b2c111a02
ShayFae <ifrahnoor108@gmail.com>
2022-08-04 23:22:10 -0400

    Strings with C#

---
+++ String-formatting/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] quarters = {1, 2, 3, 4, 5};
+int[] sales = {1000, 2000, 3000, 40000};
+double[] intMixPct = {.212, .567, .734, .129};
+string str = "TestStr";
+int val = 12345;
+decimal val1 = 12345.67891m; 
+
+//D: Decimal, N: Number, F: Fixed point, G: General, E: Exponential
+Console.WriteLine("{0:D}, {0:N}, {0:F}, {0:G}", val);
+Console.WriteLine("{0:E}, {0:N}, {0:F}, {0:G}", val1);
+
+//You can specify the amount you want 
+Console.WriteLine("{0:D6}, {0:N2}, {0:F1}, {0:G3}", val);
+
+Console.WriteLine("{0,12} {1,12} {2,12} {3,12}", quarters[0], quarters[1], quarters[2], quarters[3]);
+Console.WriteLine("{0,12:C0} {1,12:C0} {2,12:C0} {3,12:C0}", sales[0], sales[1], sales[2], sales[3]);
+Console.WriteLine("{0,12:P0} {1,12:P0} {2,12:P1} {3,12:P2}", intMixPct[0], intMixPct[1], intMixPct[2], intMixPct[3]);
+
---
+++ String-operations/Program.cs
@@ -0,0 +1,44 @@
+﻿string outstr;
+
+string str = "This is a string";
+string str1 = "THIS IS A STRING";
+string str2 = "This is a much longer string";
+
+string[] strArr = {"one", "two", "three", "four", "five"};
+
+Console.WriteLine(str.Length);
+
+Console.WriteLine(str[14]);
+
+foreach(char s in str) {
+  Console.Write(s);
+  if (s == 's') {
+    Console.WriteLine();
+    break;
+  }
+}
+
+outstr = String.Concat(strArr);
+Console.WriteLine(outstr);
+
+outstr = String.Join(".", strArr);
+Console.WriteLine(outstr);
+
+outstr = String.Join("---", strArr);
+Console.WriteLine(outstr);
+
+int result = String.Compare(str1, "This is a string");
+Console.WriteLine(result);
+
+bool isEqual = str1.Equals(str2);
+
+Console.WriteLine(isEqual);
+
+Console.WriteLine(str.IndexOf('s'));
+Console.WriteLine(str.IndexOf("is"));
+Console.WriteLine(str.LastIndexOf("string"));
+Console.WriteLine(str.LastIndexOf("a"));
+
+outstr = str.Replace("a", "not");
+Console.WriteLine(outstr);
+Console.WriteLine(outstr.IndexOf('a'));
