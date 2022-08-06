11cbd1a868d62f9542aeb9d3722cfb7eb538802d
ShayFae <ifrahnoor108@gmail.com>
2022-08-06 02:00:36 -0400

    Guess game

---
+++ Challenge1/Program.cs
@@ -0,0 +1,34 @@
+﻿int theNumber = new Random().Next(20);
+
+Console.WriteLine("Let's Play 'Guess the Number'!");
+Console.WriteLine("I'm thinking of a number between 0 and 20");
+Console.WriteLine("Enter your guess, or -1 to give up.");
+
+int guessParse = 0;
+bool check = true;
+
+do {
+  Console.WriteLine("What's your guess?");
+  string guess = Console.ReadLine();
+  guessParse = int.Parse(guess);
+
+  if(guessParse == theNumber) {
+      Console.WriteLine("That's the correct answer!");
+      check = false;
+  }
+//   else if(guessParse < theNumber) 
+// {
+//   Console.WriteLine("Higher");
+// }
+// else if(guessParse > theNumber) 
+// {
+//   Console.WriteLine("Lower");
+// }
+  else if(guessParse == -1) 
+  {
+    check = false;
+  } else {
+    Console.WriteLine("{0}", guessParse < theNumber ? "Higher" : "Lower");
+  }
+}
+while(check);
