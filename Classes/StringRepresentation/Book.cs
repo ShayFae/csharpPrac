acb6d1dd19b7d954b16d2dc201ffe5f24e8a92d5
ShayFae <ifrahnoor108@gmail.com>
2022-08-07 18:59:19 -0400

    String Representation

---
+++ Classes/StringRepresentation/Book.cs
@@ -0,0 +1,28 @@
+namespace StringRepresentation
+{
+  class Book { 
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public int PageCount { get; }
+
+    public Book(string name, string author, int pages) {
+      Name = name;
+      Author = author;
+      PageCount = pages;
+    }
+
+    public override string ToString() {
+      return $"Book {Name} by {Author}";
+    }
+
+    public string ToString(string format) {
+      if (format == "B") {
+        return $"Book: {Name}: {Author}";
+      }
+       if (format == "F") {
+        return $"Book: {Name} by {Author} is {PageCount} pages";
+      }
+      return ToString();
+    }
+  }
+}
