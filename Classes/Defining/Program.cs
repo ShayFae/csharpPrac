6a53836f3ea1c6af971041aa1396a95d0520c5a9
ShayFae <ifrahnoor108@gmail.com>
2022-08-07 03:48:09 -0400

    Classes Defining

---
+++ Classes/Defining/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Defining 
+{
+  class Program 
+  {
+    static void Main(string[] args) 
+    {
+
+      Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+      Book b2 = new Book("The Grapes of Wrath", "John Steinbeck", 464);
+
+      Console.WriteLine(b1.GetDescription());
+      Console.WriteLine(b2.GetDescription());
+
+      b1._name = "Aldous Huxley";
+
+
+    }
+  }
+}
--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,14 +1,33 @@
 ﻿namespace Palindromes {
   class Program {
     // Console.WriteLine("Let's begin:");
-    string test = "";
     int lens = 0;
-    string write = Console.ReadLine();
+    bool y = false;
+    string j = "True";
+    string m = "False";
     static void Main(string[] args) {
-    (bool, int) result = Palindrome(true, "asdasdasd");
+      string userStr = Console.ReadLine();
+      Console.WriteLine(userStr.Reverse());
+      var reverseText = string.Join("", userStr.ToLower().Reverse());
+       if (reverseText == userStr) {
+        !y;
+        n = j;
+       }
+
+      (bool, int) result = Palindrome(n, userStr);
+      
+      // if (myArr = userStr) {
+      //   Console.WriteLine("Yes");
+      // }
       Console.WriteLine($"Palindrome: {result.Item1}, Length: {result.Item2}");
     }
-    static (bool, int) Palindrome(bool check, string len) {
+    static (bool, int) Palindrome(string check, string len) {
+      //  var reverseText = string.Join("", find.ToLower().Reverse());
+      //  if (reverseText == find) {
+      //   Console.WriteLine("Yes");
+      //  }
+        // return reverseText == find;
+     
       // string guess = Console.ReadLine();
       // test = guess.Replace(" ", "");
       // Console.WriteLine(test);
@@ -16,4 +35,3 @@
     }
   }
 }
-
