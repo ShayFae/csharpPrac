1b575307a4e08e8ec45b2981edd5503cd32ef085
ShayFae <ifrahnoor108@gmail.com>
2022-08-08 10:00:02 -0400

    add boolean and remove space check

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,36 +1,24 @@
 ﻿namespace Palindromes {
   class Program {
     // Console.WriteLine("Let's begin:");
-    int lens = 0;
-    bool y = false;
-    string j = "True";
-    string m = "False";
+ 
     static void Main(string[] args) {
-      string userStr = Console.ReadLine();
-      Console.WriteLine(userStr.Reverse());
-      var reverseText = string.Join("", userStr.ToLower().Reverse());
-       if (reverseText == userStr) {
-        !y;
-        n = j;
+    bool find;
+    string userStr = Console.ReadLine().Replace(" ", "");
+
+    var reverseText = string.Join("", userStr.ToLower().Reverse());
+    var removeSpace = reverseText.Replace(" ", "");
+    Console.WriteLine(removeSpace);
+      if (removeSpace == userStr) {
+        find = true;
+       } else {
+        find = false;
        }
 
-      (bool, int) result = Palindrome(n, userStr);
-      
-      // if (myArr = userStr) {
-      //   Console.WriteLine("Yes");
-      // }
-      Console.WriteLine($"Palindrome: {result.Item1}, Length: {result.Item2}");
+    (bool, int) result = Palindrome(find, userStr);
+    Console.WriteLine($"Palindrome: {result.Item1}, Length: {result.Item2}");
     }
-    static (bool, int) Palindrome(string check, string len) {
-      //  var reverseText = string.Join("", find.ToLower().Reverse());
-      //  if (reverseText == find) {
-      //   Console.WriteLine("Yes");
-      //  }
-        // return reverseText == find;
-     
-      // string guess = Console.ReadLine();
-      // test = guess.Replace(" ", "");
-      // Console.WriteLine(test);
+    static (bool, int) Palindrome(bool check, string len) {
       return(check, len.Length);
     }
   }
